b65cab6826d0fac0b144ff52b1d9db4b2aacc4af
Muhammet Enes Şahin <030722041@std.izu.edu.tr>
2026-07-01 17:42:40 +0300

    projeyi canlıya alma

--- server.ts
+++ server.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-
+import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 import productRouter from "./routes/Product";
 import categoryRouter from "./routes/Categories";
 import orderRouter from "./routes/Orders";
